39ff02ec0b40863e4bef723be7659a050fcd022e
ni3rav <mahetan35@gmail.com>
2025-02-19 13:39:06 +0530

    add: implement Hero and Footer components, update App to include them, and add Navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,13 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
 }
