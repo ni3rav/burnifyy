790be961e18adb4c81ad8fafb752b3233d0cee16
ni3rav <mahetan35@gmail.com>
2025-02-20 21:25:38 +0530

    feat: add GEMINI_API_KEY to environment schema for configuration

--- client/lib/env.ts
+++ client/lib/env.ts
@@ -3,10 +3,12 @@ import zod from "zod";
 const envSchema = zod.object({
   NEXT_PUBLIC_BACKEND_URL: zod.string().nonempty(),
   NEXT_PUBLIC_LOGIN_URL: zod.string().nonempty(),
+  GEMINI_API_KEY: zod.string().nonempty()
 });
 
 
 export const env = envSchema.parse({
   NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   NEXT_PUBLIC_LOGIN_URL: process.env.NEXT_PUBLIC_LOGIN_URL,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
 });
