8bb157472d27bcb4a65b25cc30e5f2a507243789
ni3rav <mahetan35@gmail.com>
2025-02-22 13:11:46 +0530

    feat: update LoginButton styles for improved responsiveness and accessibility

--- client/components/LoginButton.tsx
+++ client/components/LoginButton.tsx
@@ -4,9 +4,9 @@ import { env } from "@/lib/env";
 
 function LoginButton() {
   return (
-    <a className="w-1/12" href={env.NEXT_PUBLIC_LOGIN_URL}>
-      <Button className="w-full text-black font-bold hover:bg-primary/75 transition duration-300">
-        Get Started
+    <a className="w-full sm:w-1/2 md:w-1/5 lg:w-1/4 xl:w-1/5" href={env.NEXT_PUBLIC_LOGIN_URL}>
+      <Button className="w-full text-zinc-900 font-bold hover:bg-primary/75 transition duration-300">
+      Get Started
       </Button>
     </a>
   );
