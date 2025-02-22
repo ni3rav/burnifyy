7dfb1d905cbe74e2cf475c2aa28d29882b8bb259
ni3rav <mahetan35@gmail.com>
2025-02-22 13:06:46 +0530

    feat: add favicon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {IBM_Plex_Sans} from 'next/font/google'
+import { IBM_Plex_Sans } from "next/font/google";
 import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description: "get roasted for your music taste",
 };
 
-const googleFonts = IBM_Plex_Sans({ subsets: ['latin'], weight: '400' })
+const googleFonts = IBM_Plex_Sans({ subsets: ["latin"], weight: "400" });
 
 export default function RootLayout({
   children,
@@ -17,11 +17,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </head>
       <body
         className={`${googleFonts.className} antialiased overflow-x-hidden bg-background`}
       >
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
